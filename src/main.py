3228c087d4a6d2e6c69cdd4392207888613ded6a
alnour93 <alnour-1993@hotmail.com>
2024-07-28 07:26:31 +0400

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,22 +1,52 @@
-from utils.utils import extract_mrz_from_image, extract_mrz_type1_data, extract_mrz_type2_data, extract_mrz_type3_data, display_formatted_data
+import os
+import cv2
+import numpy as np
+import easyocr
+from passporteye import read_mrz
+from utils.utils import parse_date, clean, get_gender, print_data, process_mrz_type1, process_mrz_type2, process_mrz_type3
 
-def process_image(img_path):
+# Load OCR engine (easyOCR)
+reader = easyocr.Reader(lang_list=['en'], gpu=True)  # Enable GPU if available
+
+def ocr(img_name):
     """
-    Processes an image to extract MRZ data and print the extracted information.
+    Extracts and processes personal information from the passport image.
+
+    Parameters:
+    - img_name (str): Path to the passport image.
+
+    Returns:
+    - dict: Extracted personal information.
     """
-    lines = extract_mrz_from_image(img_path)
     user_info = {}
+    temp_image_path = 'tmp.png'
+
+    # Extract MRZ from image
+    mrz = read_mrz(img_name, save_roi=True)
+    if not mrz:
+        print(f'Machine cannot read image {img_name}.')
+        return user_info
+
+    # Save and process MRZ image
+    mpimg.imsave(temp_image_path, mrz.aux['roi'], cmap='gray')
+    img = cv2.imread(temp_image_path)
+    img = cv2.resize(img, None, fx=2, fy=2)  # Increase resolution for better OCR
+
+    # Define allowed characters for OCR
+    allowlist = st.ascii_letters + st.digits + '< '
+    lines = reader.readtext(img, paragraph=False, detail=0, allowlist=allowlist)
 
     # Process MRZ lines based on their structure
     if len(lines) == 3 and all(len(line) >= 30 for line in lines):
-        user_info = extract_mrz_type1_data(lines)
+        user_info = process_mrz_type1(lines)
     elif len(lines) == 2:
         if all(len(line) >= 36 for line in lines):
-            user_info = extract_mrz_type2_data(lines)
+            user_info = process_mrz_type2(lines)
         elif all(len(line) >= 44 for line in lines):
-            user_info = extract_mrz_type3_data(lines)
+            user_info = process_mrz_type3(lines)
     else:
         print("Unrecognized MRZ format")
 
-    # Print user information
-    display_formatted_data(user_info)
+    # Clean up temporary image file
+    os.remove(temp_image_path)
+    return user_info
